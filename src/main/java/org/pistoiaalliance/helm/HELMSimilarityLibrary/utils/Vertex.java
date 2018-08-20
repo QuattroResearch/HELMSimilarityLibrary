014f3b8283d5127ee056ce44359d87b4598a2b03
Eva Bültel <eva.bueltel@contractors.roche.com>
2018-08-20 10:23:04 +0200

    changed equal() method of Vertex class

--- src/main/java/org/pistoiaalliance/helm/HELMSimilarityLibrary/utils/Vertex.java
+++ src/main/java/org/pistoiaalliance/helm/HELMSimilarityLibrary/utils/Vertex.java
@@ -1,14 +1,14 @@
 /*******************************************************************************
  * Copyright C 2016, QUATTRO RESEARCH GMBH
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
  * permit persons to whom the Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
  * Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
  * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
  * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
@@ -18,6 +18,7 @@ package org.pistoiaalliance.helm.HELMSimilarityLibrary.utils;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 import org.helm.notation2.parser.notation.polymer.MonomerNotation;
 
@@ -167,7 +168,12 @@ public class Vertex {
         return this.monomer.getUnit();
     }
 
-    public boolean equals(Vertex otherVertex) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vertex otherVertex = (Vertex) o;
         if (this.isRNA() != otherVertex.isRNA()) {
             return false;
         }
@@ -187,9 +193,6 @@ public class Vertex {
         if (!this.monomer.getUnit().equals(monomer2.getUnit())) {
             return false;
         }
-//		if(!this.monomer.getAnnotation().equals(monomer2.getAnnotation())) {
-//			return false;
-//		}
         if (!this.monomer.getCount().equals(monomer2.getCount())) {
             return false;
         }
